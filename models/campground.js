f684f005df2d0f687d4218de93cc5d33bba3f010
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-30 11:34:18 +0530

    Delete via middleware

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const campgroundSchema = new Schema({
     title: String,
@@ -14,5 +15,16 @@ const campgroundSchema = new Schema({
         }
     ]
 });
+
+// Delete via middleware
+campgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', campgroundSchema);
