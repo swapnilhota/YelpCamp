3117b69c8573a0689a7376b33facc456c63b34af
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-02 10:59:50 +0530

    Flash Styling

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -55,6 +55,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'Successfully updated campground');
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
