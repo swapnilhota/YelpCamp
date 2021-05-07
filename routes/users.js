94a5a655268aa933a83f90ce4fc65492c39adbda
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-07 10:30:48 +0530

    added users controller

--- routes/users.js
+++ routes/users.js
@@ -3,43 +3,16 @@ const passport = require('passport');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+router.get('/register', users.renderRegister);
 
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, (err) => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
+router.post('/register', catchAsync(users.register));
 
-}));
+router.get('/login', users.renderLogin);
 
-router.get('/login', (req, res) => {
-    res.render('users/login');
-});
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'Welcome Back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-});
-
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', 'Logged Out');
-    res.redirect('/campgrounds');
-})
+router.get('/logout', users.logout);
 
 module.exports = router;
