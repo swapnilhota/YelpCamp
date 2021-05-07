2db014e31a7228629c6680deab30f75c6b8e9eb7
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-07 10:02:25 +0530

    updateCampground controller

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -41,3 +41,10 @@ module.exports.renderEditForm = async (req, res) => {
     }
     res.render('campgrounds/edit', { campground });
 }
+
+module.exports.updateCampground = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'Successfully updated campground');
+    res.redirect(`/campgrounds/${campground._id}`)
+}
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,12 +15,7 @@ router.get('/:id', catchAsync(campgrounds.showCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'Successfully updated campground');
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
 router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
