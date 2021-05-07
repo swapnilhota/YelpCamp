41242814236451620ab4af870e50ba35c88a20bd
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-07 09:29:49 +0530

    added campgrounds index controller

---
+++ controllers/campgrounds.js
@@ -0,0 +1,6 @@
+const Campground = require('../models/campground');
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+}
