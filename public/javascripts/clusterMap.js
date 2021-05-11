11682315f8ade810eb39366ecca103f0aaa764af
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-11 11:03:08 +0530

    clusterMap styling

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -34,20 +34,20 @@ map.on('load', function () {
             'circle-color': [
                 'step',
                 ['get', 'point_count'],
-                '#51bbd6',
-                100,
-                '#f1f075',
-                750,
-                '#f28cb1'
+                '#03A9F4',
+                10,
+                '#2196F3',
+                30,
+                '#3F51B5'
             ],
             'circle-radius': [
                 'step',
                 ['get', 'point_count'],
+                15,
+                10,
                 20,
-                100,
                 30,
-                750,
-                40
+                25
             ]
         }
     });
